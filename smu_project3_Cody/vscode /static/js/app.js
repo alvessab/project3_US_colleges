3cc0497b616045f4d48a7b797f1963ad69c5ff4c
CodyGunter <codyjgunter2@gmail.com>
2022-04-17 18:18:31 -0700

    updated colors and nav bar

--- smu_project3_Cody/vscode /static/js/app.js
+++ smu_project3_Cody/vscode /static/js/app.js
@@ -74,7 +74,10 @@ function makePlot(schools, career_median_salary) {
         x: career_median_salary,
         y: schools,
         name: 'School vs Career Salary',
-        orientation: 'h'
+        orientation: 'h',
+        marker: {
+            color: "#740404"
+        }
     }
 
     var data1 = [trace1];
@@ -92,7 +95,8 @@ function makePlot(schools, career_median_salary) {
                 text: 'School',
             },
             automargin: true
-        }
+        },
+        height: 600
     };
 
 
@@ -121,7 +125,7 @@ function makeSunburst(sub) {
         labels: labels,
         parents: parents,
         values: values,
-        hoverinfo: 'label+percent+value',
+        hoverinfo: 'label',
         outsidetextfont: { size: 20, color: "#377eb8" },
         leaf: { opacity: 0.4 },
         marker: { line: { width: 2 } }
@@ -170,12 +174,12 @@ function makeDonut(sub) {
         title: 'School Type Breakdown',
         annotations: [{
             font: {
-                size: 9.5
+                size: 13
             },
             textinfo: "label+percent+value",
             showarrow: false,
             text: 'School Type',
-            x: 0.17,
+            x: 0.15,
             y: 0.5
         }],
         height: 500,
@@ -188,89 +192,3 @@ function makeDonut(sub) {
 
     Plotly.newPlot('scatter', data, layout);
 }
-
-
-
-// function makeChart()
-// var arr = global_data.map(x => x["Region"])
-
-// const count = {};
-
-
-// for (let i = 0; i < arr.length; i++) {
-//     const [element] = arr[i];
-
-//     if (count[element]) {
-//         count[element] += 1
-//     } else {
-//         count[element] = 1
-//     }
-// }
-
-// console.log(count)
-
-
-// var data2 = [{
-//     values: count,
-//     labels2: ["C", 'M', "N", 'S', "W"],
-//     domain: { column: 0 },
-//     name: 'College Locations',
-//     hoverinfo: 'label+percent+name',
-//     hole: .4,
-//     type: 'pie'
-// }];
-
-// var layout2 = {
-//     title: 'Global Emissions 1990-2011',
-//     annotations: [{
-//         font: {
-//             size: 20
-//         },
-//         showarrow: false,
-//         text: 'GHG',
-//         x: 0.17,
-//         y: 0.5
-//     }],
-//     height: 400,
-//     width: 600,
-//     showlegend: false,
-//     grid: { rows: 1, columns: 2 }
-// };
-
-// Plotly.newPlot('scatter', data2, layout2);
-
-
-
-
-
-
-// california = [];
-// west = [];
-// midwest = [];
-// south = [];
-// northeast = [];
-
-// for (let i = 0; i < global_data.length; i++) {
-//     // Variable to hold current school in loop
-//     let school = global_data[i]
-
-
-//     if (global_data.Region == 'California') {
-//         california.push();
-//     } else if (global_data.Region == "Western") {
-//         west.push(school);
-//     } else if (global_data.Region == "Midwestern") {
-//         midwest.push(school);
-//     } else if (global_data.Region == "Southern") {
-//         south.push(school);
-//     } else {
-//         northeast.push(school);
-//     }
-// }
-
-// console.log("---------");
-// console.log(california);
-// console.log(west);
-// console.log(midwest);
-// console.log(south);
-// console.log(northeast);
