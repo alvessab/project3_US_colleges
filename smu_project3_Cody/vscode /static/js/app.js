47a50b081e10d39267d9c4a397fe385788f011e1
CodyGunter <91342251+CodyGunter@users.noreply.github.com>
2022-04-17 18:22:00 -0700

    Add files via upload

--- smu_project3_Cody/vscode /static/js/app.js
+++ smu_project3_Cody/vscode /static/js/app.js
@@ -74,16 +74,31 @@ function makePlot(schools, career_median_salary) {
         x: career_median_salary,
         y: schools,
         name: 'School vs Career Salary',
-        orientation: 'h'
+        orientation: 'h',
+        marker: {
+            color: "#740404"
+        }
     }
 
     var data1 = [trace1];
     var layout = {
-        "title": "Bar Chart Placeholder",
-        "yaxis": {
+        title: {
+            text: 'Career Median Salary Vs School',
+        },
+        xaxis: {
+            title: {
+                text: 'Career Median Salary $(thousands)',
+            },
+        },
+        yaxis: {
+            title: {
+                text: 'School',
+            },
             automargin: true
-        }
-    }
+        },
+        height: 600
+    };
+
 
     Plotly.newPlot('bar', data1, layout);
 }
@@ -110,15 +125,18 @@ function makeSunburst(sub) {
         labels: labels,
         parents: parents,
         values: values,
+        hoverinfo: 'label',
         outsidetextfont: { size: 20, color: "#377eb8" },
         leaf: { opacity: 0.4 },
-        marker: { line: { width: 2 } },
+        marker: { line: { width: 2 } }
     }];
 
     var layout = {
         margin: { l: 0, r: 0, b: 0, t: 0 },
         width: 500,
-        height: 500
+        height: 500,
+        sunburstcolorway: ["#0c6509", "#7e5b06", "#740404", "#02285c", "#6f6161"]
+
     };
 
 
@@ -146,9 +164,9 @@ function makeDonut(sub) {
         values: values,
         labels: labels,
         domain: { column: 0 },
-        name: 'GHG Emissions',
-        hoverinfo: 'label+percent',
+        hoverinfo: 'label+percent+value',
         hole: .4,
+
         type: 'pie'
     }];
 
@@ -156,106 +174,21 @@ function makeDonut(sub) {
         title: 'School Type Breakdown',
         annotations: [{
             font: {
-                size: 9.5
+                size: 13
             },
-            textinfo: "label+percent+name",
+            textinfo: "label+percent+value",
             showarrow: false,
-            text: ' School Type',
-            x: 0.13,
+            text: 'School Type',
+            x: 0.15,
             y: 0.5
         }],
-        height: 400,
-        width: 600,
+        height: 500,
+        width: 750,
+        colorway: ["#0c6509", "#7e5b06", "#740404", "#02285c", "#6f6161"],
         showlegend: true,
         grid: { rows: 1, columns: 2 },
-        //colorway: c("#0c6509", "#7e5b06", "#740404", "#02285c", "#6f6161")
+
     };
 
     Plotly.newPlot('scatter', data, layout);
 }
-
-
-
-// function makeChart()
-// var arr = global_data.map(x => x["Region"])
-
-// const count = {};
-
-
-// for (let i = 0; i < arr.length; i++) {
-//     const [element] = arr[i];
-
-//     if (count[element]) {
-//         count[element] += 1
-//     } else {
-//         count[element] = 1
-//     }
-// }
-
-// console.log(count)
-
-
-// var data2 = [{
-//     values: count,
-//     labels2: ["C", 'M', "N", 'S', "W"],
-//     domain: { column: 0 },
-//     name: 'College Locations',
-//     hoverinfo: 'label+percent+name',
-//     hole: .4,
-//     type: 'pie'
-// }];
-
-// var layout2 = {
-//     title: 'Global Emissions 1990-2011',
-//     annotations: [{
-//         font: {
-//             size: 20
-//         },
-//         showarrow: false,
-//         text: 'GHG',
-//         x: 0.17,
-//         y: 0.5
-//     }],
-//     height: 400,
-//     width: 600,
-//     showlegend: false,
-//     grid: { rows: 1, columns: 2 }
-// };
-
-// Plotly.newPlot('scatter', data2, layout2);
-
-
-
-
-
-
-// california = [];
-// west = [];
-// midwest = [];
-// south = [];
-// northeast = [];
-
-// for (let i = 0; i < global_data.length; i++) {
-//     // Variable to hold current school in loop
-//     let school = global_data[i]
-
-
-//     if (global_data.Region == 'California') {
-//         california.push();
-//     } else if (global_data.Region == "Western") {
-//         west.push(school);
-//     } else if (global_data.Region == "Midwestern") {
-//         midwest.push(school);
-//     } else if (global_data.Region == "Southern") {
-//         south.push(school);
-//     } else {
-//         northeast.push(school);
-//     }
-// }
-
-// console.log("---------");
-// console.log(california);
-// console.log(west);
-// console.log(midwest);
-// console.log(south);
-// console.log(northeast);
